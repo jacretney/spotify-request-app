3d18b23e597fe800b829f6ecac684126f3007f43
Jim Cretney <jacretney@gmail.com>
2020-10-17 18:43:09 +0100

    Display songs from spotify

--- src/components/songs/SongGrid.js
+++ src/components/songs/SongGrid.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const SongGrid = ({ isLoading, songs }) => {
+  const parseSongData = (song) => {
+    return {
+      id: song.id,
+      img: song.album.images[0].url, // Highest quality image
+      title: song.title,
+      artist: song.artist.name,
+    }
+  }
+
   const showSongs = (songs) => {
     if (songs.length === 0) {
       return (
@@ -10,11 +19,13 @@ const SongGrid = ({ isLoading, songs }) => {
       )
     }
     return songs.map((song) => {
+      const parsed = parseSongData(song);
+
       return (
-        <div className="song-card" key={song.id}>
-          <img src={song.img} alt={`${song.title} by ${song.artist}`}/>
-          <h3>{song.title}</h3>
-          <h4>{song.artist}</h4>
+        <div className="song-card" key={parsed.id}>
+          <img src={parsed.img} alt={`${parsed.title} by ${parsed.artist}`}/>
+          <h3>{parsed.title}</h3>
+          <h4>{parsed.artist}</h4>
         </div>
       )
     });
